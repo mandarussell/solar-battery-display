e7691dfa53e57798b54a2f61e1cc9ea411e3b5a1
Manda Russell <hello@amandarussell.me.uk>
2026-04-30 12:14:30 +0100

    Added time charge settings.

--- src/TimedCharge.js
+++ src/TimedCharge.js
@@ -22,12 +22,37 @@ function TimedCharge() {
 
     return (
         <div>
-            <label className="toggle-switch">
-                <input type="checkbox" className="checkbox" defaultChecked={response.data.enabled} />
-                <span className="slider"></span>
-            </label>
-            <span className="text">Timed Charge Enabled</span>
-        </div >
+            <h1>Timed Charge</h1>
+            <div>
+                <label className="toggle-switch">
+                    <input type="checkbox" className="checkbox" defaultChecked={response.data.enabled} />
+                    <span className="slider"></span>
+                </label>
+                <span className='time-input'>
+                    <input
+                        type="time"
+                        className="start-time"
+                        defaultValue={response.data.slots[0].start_time}
+                        disabled={response.data.enabled === false ? true : false}
+                    ></input>
+                    <input
+                        type="time"
+                        className="end-time"
+                        defaultValue={response.data.slots[0].end_time}
+                        disabled={response.data.enabled === false ? true : false}
+                    ></input>
+                </span>
+                <span>
+                    <input
+                        type="number"
+                        className="charge-limit"
+                        defaultValue={response.data.slots[0].percent_limit}
+                        disabled={response.data.enabled === false ? true : false}
+                    ></input>
+                    %
+                </span>
+            </div>
+        </div>
     );
 }
 
